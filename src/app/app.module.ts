b0b77626f7c9cdf0f36b591dac04f807f9933427
Dhany <dhanybasuki@ncs.com.sg>
2017-08-16 17:24:03 +0800

    add material dependency + version upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpModule } from "@angular/http";
 
-import { AppComponent } from './app.component';
-import { Safe } from './safe';
+import { AppComponent } from "./app.component";
+import { Safe } from "./safe";
 
-import { BookStoreService } from './books';
+import { BookStoreService } from "./books";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Safe
-  ],
-  imports: [
-    BrowserModule,
-    HttpModule
-  ],
+  declarations: [AppComponent, Safe],
+  imports: [BrowserModule, HttpModule],
   providers: [BookStoreService],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
