3fea8679f8eb146b1ded9190fbda93f60b77651d
Dhany <dhanybasuki@ncs.com.sg>
2017-08-16 17:10:50 +0800

    update book source from own mockapi.io

--- src/app/books/book-store.service.ts
+++ src/app/books/book-store.service.ts
@@ -9,7 +9,7 @@ import { Book } from './book';
 @Injectable()
 export class BookStoreService {
 
-  baseUrl = 'http://58e15045f7d7f41200261f77.mockapi.io/api/v1/books/';
+  baseUrl = 'http://59940aa9d297ba0011da1aee.mockapi.io/books';
   headers = new Headers({'Content-Type': 'application/json'});
 
   constructor(private http: Http) {
