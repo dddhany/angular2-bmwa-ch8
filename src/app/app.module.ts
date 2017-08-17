caca369ff5997f984e30c2e25bef7e99e4ba42d9
Dhany <dhanybasuki@ncs.com.sg>
2017-08-17 09:43:54 +0800

    add flex layout module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from "@angular/core";
 import { HttpModule } from "@angular/http";
 
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppComponent } from "./app.component";
 import { Safe } from "./safe";
@@ -11,7 +12,12 @@ import { BookStoreService } from "./books";
 
 @NgModule({
   declarations: [AppComponent, Safe],
-  imports: [BrowserModule, HttpModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule
+  ],
   providers: [BookStoreService],
   bootstrap: [AppComponent]
 })
