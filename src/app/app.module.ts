01a34eb39c192dc1d92e6a5ca3cb9c3626cadf98
Dhany <dhanybasuki@ncs.com.sg>
2017-08-17 09:41:19 +0800

    add material hammerjs and googlefont

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpModule } from "@angular/http";
 
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
 import { AppComponent } from "./app.component";
 import { Safe } from "./safe";
 
@@ -9,7 +11,7 @@ import { BookStoreService } from "./books";
 
 @NgModule({
   declarations: [AppComponent, Safe],
-  imports: [BrowserModule, HttpModule],
+  imports: [BrowserModule, HttpModule, BrowserAnimationsModule],
   providers: [BookStoreService],
   bootstrap: [AppComponent]
 })
